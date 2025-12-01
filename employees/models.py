ecfd31c9ebb2eff9e34f162f3994d949c451e2a8
Eugene <eugyneehunter@gmail.com>
2025-12-01 14:26:48 +0300

    feat: Initialize Django project with an employees app, Employee model, and media file configuration.

--- employees/models.py
+++ employees/models.py
@@ -11,3 +11,6 @@ class Employee(models.Model):
     phone_number = models.CharField(max_length=15, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id) + " " + self.first_name + " " + self.last_name
