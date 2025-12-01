e50109ce2178b30a3da0776fd1a2d74b2b749ef5
Eugene <eugyneehunter@gmail.com>
2025-12-01 14:32:02 +0300

    feat: Add Employee model with personal and contact details, including photo upload.

--- employees/models.py
+++ employees/models.py
@@ -13,4 +13,12 @@ class Employee(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return str(self.id) + " " + self.first_name + " " + self.last_name
+        return (
+            str(self.id)
+            + " "
+            + self.first_name
+            + " "
+            + self.last_name
+            + " "
+            + str("(" + self.designation + ")")
+        )
